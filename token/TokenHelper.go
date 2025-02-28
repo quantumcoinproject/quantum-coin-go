8d5c22b60ffa6748324402dcf4a29c47d59822fc
Blake Hartin <bh@bh>
2025-02-27 17:37:08 -0800

    tokenhelper

--- token/TokenHelper.go
+++ token/TokenHelper.go
@@ -6,6 +6,7 @@ import (
 	"github.com/QuantumCoinProject/qc/common"
 	"github.com/QuantumCoinProject/qc/core/types"
 	"github.com/QuantumCoinProject/qc/crypto"
+	"github.com/QuantumCoinProject/qc/log"
 	"math/big"
 	"strings"
 )
@@ -17,8 +18,6 @@ var LogApprovalSig = []byte("Approval(address,address,uint256)")
 var logTransferSigHash = strings.ToLower(crypto.Keccak256Hash(logTransferSig).Hex())
 var logApprovalSigHash = strings.ToLower(crypto.Keccak256Hash(LogApprovalSig).Hex())
 
-var InvalidTokenLog = errors.New("invalid token log")
-
 type TokenDetails struct {
 	Name        string
 	Symbol      string
@@ -27,64 +26,78 @@ type TokenDetails struct {
 	Decimals    uint8
 }
 
+type LogTransferValue struct {
+	Value *big.Int
+}
+
 type LogTransfer struct {
-	From   common.Address
-	To     common.Address
-	Tokens *big.Int
+	ContractAddress common.Address
+	From            common.Address
+	To              common.Address
+	Tokens          *big.Int
 }
 
 type LogApproval struct {
-	TokenOwner common.Address
-	Spender    common.Address
-	Tokens     *big.Int
+	ContractAddress common.Address
+	TokenOwner      common.Address
+	Spender         common.Address
+	Tokens          *big.Int
 }
 
-func GetAccountsInvolvedOfTransaction(txn *types.Transaction, receipt *types.Receipt) ([]*LogTransfer, []*LogApproval, error) {
-	if txn == nil || receipt == nil || txn.To() == nil {
-		return nil, nil, errors.New("not a token contract")
+func ParseTokenTransaction(txn *types.Transaction, receipt *types.Receipt) ([]*LogTransfer, []*LogApproval, error) {
+	if txn == nil || receipt == nil {
+		return nil, nil, errors.New("txn or receipt is nil")
 	}
 
 	txHash := txn.Hash()
 	if txHash.IsEqualTo(receipt.TxHash) == false {
-		return nil, nil, errors.New("hash mismatch")
+		return nil, nil, errors.New("hash mismatch between txn and receipt")
 	}
 
 	transfers := make([]*LogTransfer, 0)
 	approvals := make([]*LogApproval, 0)
 
 	for _, rLog := range receipt.Logs {
+
 		switch strings.ToLower(rLog.Topics[0].Hex()) {
 
 		case logTransferSigHash:
 			var transferEvent LogTransfer
+			var logTransferValue LogTransferValue
 
-			err := contractAbi.UnpackIntoInterface(&transferEvent, "Transfer", rLog.Data)
+			err := contractAbi.UnpackIntoInterface(&logTransferValue, "Transfer", rLog.Data)
 			if err != nil {
-				return nil, nil, InvalidTokenLog
+				log.Debug("ParseTokenTransaction Transfer", "error", err)
+				//do not return from error, since from/to address have to be updated still for balance update
+			} else {
+				transferEvent.Tokens = logTransferValue.Value
 			}
 
 			transferEvent.From = common.HexToAddress(rLog.Topics[1].Hex())
 			transferEvent.To = common.HexToAddress(rLog.Topics[2].Hex())
 
+			transferEvent.ContractAddress = rLog.Address
+
 			transfers = append(transfers, &transferEvent)
 
 		case logApprovalSigHash:
 			var approvalEvent LogApproval
+			var logTransferValue LogTransferValue
 
-			err := contractAbi.UnpackIntoInterface(&approvalEvent, "Approval", rLog.Data)
+			err := contractAbi.UnpackIntoInterface(&logTransferValue, "Approval", rLog.Data)
 			if err != nil {
-				return nil, nil, InvalidTokenLog
+				log.Debug("ParseTokenTransaction Approval", "error", err)
+				//do not return from error, since from/to address have to be updated still for balance update
+			} else {
+				approvalEvent.Tokens = logTransferValue.Value
 			}
 
+			approvalEvent.ContractAddress = rLog.Address
 			approvalEvent.TokenOwner = common.HexToAddress(rLog.Topics[1].Hex())
 			approvalEvent.Spender = common.HexToAddress(rLog.Topics[2].Hex())
 
 			approvals = append(approvals, &approvalEvent)
 		}
-	}
-
-	if len(transfers) > 0 || len(approvals) > 0 {
-		panic("done")
 	}
 
 	return transfers, approvals, nil
