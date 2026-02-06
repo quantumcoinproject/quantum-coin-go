f1c2c2b8c14f7538563fe2190e7af9d7ae6a2c31
blakehartin <bh@bh>
2026-02-05 17:25:17 -0800

    remove relayv2

--- cachemanager/common.go
+++
@@ -1,9 +0,0 @@
-package cachemanager
-
-func getPageCount(itemCount uint64) uint64 {
-	if itemCount%PageSize == 0 {
-		return itemCount / PageSize
-	} else {
-		return (itemCount / PageSize) + 1
-	}
-}
--- cachemanager/staking_models.go
+++
@@ -1,6 +0,0 @@
-package cachemanager
-
-type StakingReport struct {
-	TotalStakedCoins string `json:"totalStakedCoins,omitempty"`
-	ReportDate       int64  `json:"reportDate,omitempty"`
-}
