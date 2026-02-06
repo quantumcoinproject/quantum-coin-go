98007ba49616e0881b0896ae146b23ed5cf1266d
blakehartin <bh@bh>
2026-02-06 14:22:34 -0800

    validator debug trace normalize

--- consensus/proofofstake/filterValidators.go
+++ consensus/proofofstake/filterValidators.go
@@ -44,7 +44,7 @@ func filterValidators(consensusContext common.Hash, valDepMap *map[common.Addres
 				validatorsDepositMap[val] = depositValue
 			}
 		}
-		log.Trace("filterValidators before normalizeDeposit", "val", val, "depositValue", depositValue, "block", blockNumber)
+		log.Debug("filterValidators before normalizeDeposit", "val", val, "depositValue", depositValue, "block", blockNumber)
 		totalDepositValue = common.SafeAddBigInt(totalDepositValue, depositValue)
 		valCount = valCount + 1
 	}
@@ -80,7 +80,7 @@ func filterValidators(consensusContext common.Hash, valDepMap *map[common.Addres
 	for val, _ := range filteredValidators {
 		depositValue := validatorsDepositMap[val]
 		filteredDepositValue = common.SafeAddBigInt(filteredDepositValue, depositValue)
-		log.Trace("filterValidators after normalizeDeposit", "val", val, "depositValue", depositValue, "block", blockNumber)
+		log.Debug("filterValidators after normalizeDeposit", "val", val, "depositValue", depositValue, "block", blockNumber)
 	}
 
 	if filteredDepositValue.Cmp(MIN_BLOCK_DEPOSIT) == -1 {
