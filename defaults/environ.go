ebd4f1739a66ef69efa5f1b3a0ff9fda7da2a9f4
blakehartin <bh@bh>
2026-02-07 09:35:06 -0800

    SKIP_REBROADCAST_CONSENSUS_PACKETS

--- defaults/environ.go
+++ defaults/environ.go
@@ -3,7 +3,7 @@ package defaults
 import "os"
 
 var sendStaticNodesOnly = os.Getenv("SEND_STATIC_AND_OUTBOUND_NODES_ONLY")
-var skipRebroadcastConsensusPackets = os.Getenv("SKIP_REBROADCAST_CONSENSUS_PACKET")
+var skipRebroadcastConsensusPackets = os.Getenv("SKIP_REBROADCAST_CONSENSUS_PACKETS")
 
 func SendStaticAndOutboundNodesOnly() bool {
 	return sendStaticNodesOnly == "1"
