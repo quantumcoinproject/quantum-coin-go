ba427dcf53fec71cd14dede7058d23bd81db2d4f
blakehartin <bh@bh>
2026-07-25 10:52:45 -0700

    evm capture

--- core/vm/logger_json.go
+++ core/vm/logger_json.go
@@ -81,6 +81,14 @@ func (l *JSONLogger) CaptureState(env *EVM, pc uint64, op OpCode, gas, cost uint
 	l.encoder.Encode(log)
 }
 
+// CaptureEnter is called when the EVM enters a new scope (via call, create or selfdestruct).
+func (l *JSONLogger) CaptureEnter(typ OpCode, from common.Address, to common.Address, input []byte, gas uint64, value *big.Int) {
+}
+
+// CaptureExit is called when the EVM exits a scope, even if the scope didn't
+// execute any code.
+func (l *JSONLogger) CaptureExit(output []byte, gasUsed uint64, err error) {}
+
 // CaptureEnd is triggered at end of execution.
 func (l *JSONLogger) CaptureEnd(output []byte, gasUsed uint64, t time.Duration, err error) {
 	type endLog struct {
